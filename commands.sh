23d3443d588a7bdc6d34e52bacac5a90c115a3a3
Siddhant Rath <sid@tamu.edu>
2023-03-31 14:59:12 -0500

    update

---
+++ commands.sh
@@ -0,0 +1,14 @@
+python terminateProjects.py
+python addCompoundEvolution.py
+python renameCompoundIDs.py
+python addProjects.py
+python addCompass.py
+
+python addUnpublishedStructures.py
+python addProteinProduction.py
+python addProteinAssay.py
+python addVulnerability.py
+python addEssentiality.py
+
+python addGeneGroups.py
+python addOrgs.py
